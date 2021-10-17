b172010015212ef9196d1ddeb757025eba6ea0aa
Waeytens-Jonathan <waeytensjonathan@outlook.fr>
2021-10-17 21:57:06 +0200

    Ajout de la fonction pour extraire les urls des categories

--- categories.py
+++ categories.py
@@ -1,5 +1,33 @@
+import page
 
-#def get_all_url_categories():
+
+
+
+#url = page.parse_main_page()
+
+def get_all_categories(url):
+
+    soup = page.parse_main_page(url)
+
+    categories = {}
+
+    all_categories = soup.find('div', class_='side_categories').find_all('li').text
+
+    '''for categorie in all_categories:
+        
+        categories + all_categories'''
+
+        
+
+    print(all_categories)
+
+get_all_categories("https://books.toscrape.com/")
+    
+    
+    
+    
+    
+    
     # Récupère toute les url des catégories des livres
 
 
